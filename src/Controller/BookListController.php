d684657a836ce105d027c40b0a3183f558ef8c8d
jojo <joseph.lep@gmail.com>
2024-06-21 16:10:10 +0200

    new repo

---
+++ src/Controller/BookListController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class BookListController extends AbstractController
+{
+    #[Route('/book', name: 'book')]
+    public function index(): Response
+    {
+        return $this->render('book_list/index.html.twig', [
+            'controller_name' => 'BookListController',
+        ]);
+    }
+}
